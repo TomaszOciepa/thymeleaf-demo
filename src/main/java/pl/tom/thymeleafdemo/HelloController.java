88fce0d179d9d57d1da7ba37825ba475644180ea
tomaszociepa <tom.ociepa@gmail.com>
2019-10-04 21:01:33 +0200

    thymeleaf each

--- src/main/java/pl/tom/thymeleafdemo/HelloController.java
+++ src/main/java/pl/tom/thymeleafdemo/HelloController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 public class HelloController {
 
@@ -11,9 +14,13 @@ public class HelloController {
     public String get(Model model){
 
         Car car = new Car("CLS", "Mercedes");
+        Car car1 = new Car("X5", "BMW");
+        Car car2 = new Car("WRX", "SUBARU");
+        Car car3 = new Car("911", "Porshe");
 
+        List<Car> carList = Arrays.asList(car, car1, car2, car3);
         model.addAttribute("name", "Tomek");
-        model.addAttribute("car", car);
+        model.addAttribute("cars", carList);
 
         return "hello";
     }
